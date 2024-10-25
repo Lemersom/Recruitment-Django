e6eab0b5bfbc61bb23d8aea664ab268ab42b3031
lemersom <lemerfilho@gmail.com>
2024-10-25 00:42:06 -0300

    debug: add models to admin site register

--- recruitment_api/admin.py
+++ recruitment_api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import PersonalInformation
+from .models import PersonalInformation, Contact, ProfessionalExperience, AcademicBackground
 
 admin.site.register(PersonalInformation)
+admin.site.register(Contact)
+admin.site.register(ProfessionalExperience)
+admin.site.register(AcademicBackground)
